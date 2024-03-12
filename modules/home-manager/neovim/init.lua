f09eda92b4b3568736d22bc66ef3764ed1964bca
Elijah Lieu <elijahlieu@protonmail.com>
2024-03-12 17:10:17 -0400

    157 current  2024-03-12 17:10:14  24.05.20240309.3030f18  6.6.21                          *

--- modules/home-manager/neovim/init.lua
+++ modules/home-manager/neovim/init.lua
@@ -505,12 +505,21 @@ require("lazy").setup({
 
 	{ -- Autoformat
 		"stevearc/conform.nvim",
+		config = function()
+			vim.api.nvim_create_user_command("Format", function(args)
+				local range = nil
+				if args.count ~= -1 then
+					local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+					range = {
+						start = { args.line1, 0 },
+						["end"] = { args.line2, end_line:len() },
+					}
+				end
+				require("conform").format({ async = true, lsp_fallback = true, range = range })
+			end, { range = true })
+		end,
 		opts = {
 			notify_on_error = false,
-			format_on_save = {
-				timeout_ms = 500,
-				lsp_fallback = true,
-			},
 			formatters_by_ft = {
 				lua = { "stylua" },
 				-- Conform can also run multiple formatters sequentially
