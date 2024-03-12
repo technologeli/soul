16804c693c53baa0bb4d6b494788d5b63a234f67
Elijah Lieu <elijahlieu@protonmail.com>
2024-03-12 16:46:08 -0400

    156 current  2024-03-12 16:46:04  24.05.20240309.3030f18  6.6.21                          *

--- modules/home-manager/neovim/init.lua
+++ modules/home-manager/neovim/init.lua
@@ -430,7 +430,7 @@ require("lazy").setup({
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
 				clangd = {
-					cmd = "/run/current-system/sw/bin/clangd",
+					cmd = { "/run/current-system/sw/bin/clangd" },
 				},
 				-- gopls = {},
 				-- pyright = {},
