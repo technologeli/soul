181b232c4945332fb81a0d72407e2488166117a0
Elijah Lieu <elijahlieu@protonmail.com>
2024-03-12 13:23:29 -0400

    145 current  2024-03-12 11:29:58  24.05.20240309.3030f18  6.6.21                          *

--- scripts/ctf.nix
+++ scripts/ctf.nix
@@ -5,7 +5,21 @@ in
     packages = with pkgs; [
       gef
       openssl
+      upx
+      ghidra-bin
+      unixtools.xxd
+
+      # stenography
+      tenacity
+      stegsolve
+      binwalk
+      steghide
+      zsteg
+      outguess
+      exiftool
+
       (python3.withPackages (python-pkgs: [
+        python-pkgs.angr
         python-pkgs.pycryptodome
         python-pkgs.pwntools
         python-pkgs.ropgadget
