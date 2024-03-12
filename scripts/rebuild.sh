6e42f6039fb3b93495e9c8fef52e70eb7f799bc9
Elijah Lieu <elijahlieu@protonmail.com>
2024-03-12 10:04:19 -0400

    Add scripts and log

---
+++ scripts/rebuild.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+pushd ~/soulfiles
+nvim
+alejandra . &>/dev/null
+git diff -U0 *.nix
+echo "NixOS Rebuilding..."
+rm ~/.mozilla/firefox/myprofile/containers.json
+sudo nixos-rebuild switch --upgrade --flake ~/soulfiles/#soul &>nixos-switch.log || (
+ cat nixos-switch.log | grep --color error && false)
+gen=$(nixos-rebuild list-generations | grep current)
+git commit -am "$gen"
+popd
