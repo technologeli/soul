60cd1d736afd96c4dc04ead1b32be7711c35d7e4
Elijah Lieu <elijahlieu@protonmail.com>
2024-03-18 10:22:24 -0400

    162 current  2024-03-18 10:22:21  24.05.20240309.3030f18  6.6.21                          *

--- modules/home-manager/neovim/init.lua
+++ modules/home-manager/neovim/init.lua
@@ -56,10 +56,10 @@ vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagn
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
 -- Window movement
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+-- vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+-- vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+-- vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+-- vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Switch line below" })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Switch line above" })
