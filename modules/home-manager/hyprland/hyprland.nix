d4233072a7a04cf943a5aaa17efa2d8e015e6650
Elijah Lieu <elijahlieu@protonmail.com>
2024-03-30 23:43:29 -0400

    176 current  2024-03-30 23:43:25  24.05.20240309.3030f18  6.6.21                          *

--- modules/home-manager/hyprland/hyprland.nix
+++ modules/home-manager/hyprland/hyprland.nix
@@ -239,6 +239,7 @@
             "$mainMod, Q, killactive, "
             "$mainMod, Escape, exit, "
             "$mainMod, E, exec, $fileManager"
+            "$mainMod, C, exec, $terminal -e calcurse"
             "$mainMod, V, togglefloating, "
             "$mainMod, P, exec, $menu"
 
