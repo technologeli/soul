a02d6f81623c789d614be0f1a1c2a35d478c69a6
Elijah Lieu <elijahlieu@protonmail.com>
2024-05-05 17:17:36 -0400

    199 current  2024-05-05 17:17:33  24.05.20240309.3030f18  6.6.21                          *

--- modules/home-manager/neovim/init.lua
+++ modules/home-manager/neovim/init.lua
@@ -101,6 +101,15 @@ vim.api.nvim_create_autocmd("BufWritePre", {
   end,
 })
 
+vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
+  desc = "Make .templ files have a filetype of templ",
+  group = vim.api.nvim_create_augroup("Templ", { clear = true }),
+  pattern = "*.templ",
+  callback = function()
+    vim.opt_local.filetype = "templ"
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
