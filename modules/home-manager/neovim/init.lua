4b2d59b3b30d8d4bccea45b7d73b87b3dcff9d0d
Elijah Lieu <50640682+technologeli@users.noreply.github.com>
2024-06-12 21:15:07 -0400

    10 current  2024-06-12 21:15:03  24.05.20240309.3030f18   6.6.21                          *

--- modules/home-manager/neovim/init.lua
+++ modules/home-manager/neovim/init.lua
@@ -54,6 +54,8 @@ vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 -- Diagnostic keymaps
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
+vim.keymap.set("n", "\\f", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
+vim.keymap.set("n", "\\d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
