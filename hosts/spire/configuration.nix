ff0a688e742f5f893132659562d26ced7312eed1
Elijah Lieu <50640682+technologeli@users.noreply.github.com>
2024-06-12 21:00:57 -0400

    9 current   2024-06-12 21:00:53  24.05.20240309.3030f18   6.6.21                          *

--- hosts/spire/configuration.nix
+++ hosts/spire/configuration.nix
@@ -35,28 +35,8 @@
   };
 
   services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-  environment.gnome.excludePackages =
-    (with pkgs; [
-      gnome-photos
-      gnome-tour
-      gedit # text editor
-    ])
-    ++ (with pkgs.gnome; [
-      cheese # webcam tool
-      gnome-music
-      gnome-terminal
-      epiphany # web browser
-      geary # email reader
-      evince # document viewer
-      gnome-characters
-      totem # video player
-      tali # poker game
-      iagno # go game
-      hitori # sudoku game
-      atomix # puzzle game
-    ]);
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma6.enable = true;
   # programs.hyprland = {
   #   enable = true;
   #   xwayland.enable = true;
