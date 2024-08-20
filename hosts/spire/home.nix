38fb4866dfc8cd6d2750d55842f357a30c79d162
Elijah Lieu <50640682+technologeli@users.noreply.github.com>
2024-08-20 11:19:22 -0400

    23 current  2024-08-20 11:19:20  24.11.20240818.8a33541  6.6.46                          *

--- hosts/spire/home.nix
+++ hosts/spire/home.nix
@@ -90,6 +90,7 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
+    ".icons/default".source = "${pkgs.vanilla-dmz}/share/icons/Vanilla-DMZ";
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
