88bcd23b9757baa504a3209e852deff500941262
Elijah Lieu <50640682+technologeli@users.noreply.github.com>
2024-12-16 14:43:10 -0500

    sync

--- hosts/spire/configuration.nix
+++ hosts/spire/configuration.nix
@@ -58,7 +58,16 @@
   };
   hardware = {
     graphics.enable = true;
-    # nvidia.modesetting.enable = true;
+  };
+
+  services.xserver.videoDrivers = ["nvidia"];
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
   services.xserver.xkb = {
